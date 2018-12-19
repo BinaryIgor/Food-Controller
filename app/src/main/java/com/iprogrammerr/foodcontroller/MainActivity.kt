e882e48ff958f7d3011bb63c3fc15fadb88581d3
igor <ceigor94@gmail.com>
2018-12-19 23:37:23 +0100

    Customizing Toolbar

--- app/src/main/java/com/iprogrammerr/foodcontroller/MainActivity.kt
+++ app/src/main/java/com/iprogrammerr/foodcontroller/MainActivity.kt
@@ -1,18 +1,41 @@
 package com.iprogrammerr.foodcontroller
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import com.iprogrammerr.foodcontroller.databinding.ActivityMainBinding
 import com.iprogrammerr.foodcontroller.view.RootView
 import com.iprogrammerr.foodcontroller.view.fragment.MenuFragment
 
 class MainActivity : AppCompatActivity(), RootView {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        this.binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         if (savedInstanceState == null) {
             replace(MenuFragment(), false)
+        }
+        this.binding.back.setOnClickListener { _ ->
+            if (this.supportFragmentManager.backStackEntryCount > 0)
+                onBackPressed()
+        }
+        this.supportFragmentManager.addOnBackStackChangedListener { resolveBackVisibility() }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        resolveBackVisibility()
+    }
+
+    private fun resolveBackVisibility() {
+        if (this.supportFragmentManager.backStackEntryCount == 0) {
+            this.binding.back.visibility = View.INVISIBLE
+        } else {
+            this.binding.back.visibility = View.VISIBLE
         }
     }
 
@@ -26,6 +49,6 @@ class MainActivity : AppCompatActivity(), RootView {
     }
 
     override fun changeTitle(title: String) {
-        this.supportActionBar?.title = title
+        this.binding.toolbarTitle.text = title
     }
 }
